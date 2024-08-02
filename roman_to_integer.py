60c3ae8c0100fdfb8a9ac53f85f96f7d11412515
berdiyorova <rano.baxromovna@gmail.com>
2024-08-02 10:58:47 +0500

    create app.py and roman_to_integer module

---
+++ roman_to_integer.py
@@ -0,0 +1,21 @@
+def roman_to_int(s):
+    roman = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    num = 0
+    i = len(s) - 1
+    if i == 0:
+        return roman[s[0]]
+
+    while s != '':
+
+        if i == 0 or roman[s[i]] <= roman[s[i - 1]]:
+            num += roman[s[i]]
+            s = s[:i]
+
+        elif roman[s[i]] > roman[s[i - 1]]:
+            num += roman[s[i]] - roman[s[i - 1]]
+            s = s[:i]
+            s = s[:i - 1]
+        i = len(s) - 1
+    return num
